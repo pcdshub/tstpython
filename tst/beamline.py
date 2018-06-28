a723626c023d6a70a29eaa228270df98a89452c5
Zachary Lentz <zlentz@slac.stanford.edu>
2018-06-28 11:08:55 -0700

    FIX: use sane names and get correlated sim devices for the plot

--- tst/beamline.py
+++ tst/beamline.py
@@ -1,6 +1,11 @@
 from hutch_python.utils import safe_load
-from pcdsdevices.epics_motor import IMS
 
 
-with safe_load('Example XCS Motor'):
+with safe_load('example xcs motor'):
+    from pcdsdevices.epics_motor import IMS
     xcs_user_30 = IMS('XCS:USR:MMS:30', name='xcs_user_30')
+
+
+with safe_load('coupled sim motor and det'):
+    from ophyd.sim import motor as sim_motor, det as sim_det
+    sim_det.kind = 'hinted'
